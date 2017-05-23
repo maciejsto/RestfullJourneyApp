93c00cfc67c12ee3b840339873c3e1b0ad5f7ffd
maciej <maciejsto@gmail.com>
2017-05-23 11:09:32 +0000

    added base project structure with vue generator

---
+++ app/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>{{ msg }}</h1>
+    <h2>About</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script> 
